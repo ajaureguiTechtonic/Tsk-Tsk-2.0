d7a055ddb2f2efc17a027d3e72325e37cd0efcbc
Ana Jauregui <ana.jauregui@techtonic.com>
2018-10-30 14:51:20 -0600

    templates for both higher and lower level task types

--- src/components/TaskContainer.js
+++ src/components/TaskContainer.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import LowerLevelTask from './tasks/LowerLevelTask';
+import HigherLevelTask from './tasks/HigherLevelTask';
 
 class TaskContainer extends Component{
   render() {
     return (
       <div>
         Welcome to TSK-TSK, coolest Task Management App ever!
+        <LowerLevelTask />
+        <HigherLevelTask />
       </div>
     );
   }
