ddea6733f81f431ce2425a1aed9e96486435b844
Ana Jauregui <ana.jauregui@techtonic.com>
2018-10-30 15:35:49 -0600

    clean up unused varas

--- src/components/AddTaskButton.js
+++ src/components/AddTaskButton.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import plusButton from '.././assets/add.png';
 
 const AddTaskButton = function (props) {
--- src/components/modals/AddTaskModal.js
+++ src/components/modals/AddTaskModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import {Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class AddTaskModal extends Component {
   constructor (props) {
