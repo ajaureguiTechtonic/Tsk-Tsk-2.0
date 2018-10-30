45d0266fd92a7f8ec90b98fdd8940b28fdf4bde3
Jose Hernandez-Inunza <>
2018-10-30 11:23:12 -0600

    fixed className stuff

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import '../main.css';
 import Logo from '../assets/first_logo.png';
-import '../BootstrapCSS/bootstrap.min.css';
 
 class NavBar extends Component{
   constructor() {
@@ -12,14 +11,14 @@ class NavBar extends Component{
   render() {
     return (
       <div>
-        <nav class="navbar navbar-expand navbar-light fixed-top">
-          <a class="navbar-brand" href="/"><img class="nav-logo" src={Logo} alt="tsk-tsk logo" /></a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand navbar-light fixed-top">
+          <a className="navbar-brand" href="/"><img className="nav-logo" src={Logo} alt="tsk-tsk logo" /></a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
           {/* navBtn */}
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <button type="button" class="btn edit-button ml-auto float-right" data-toggle="modal" data-target="#modal-signup">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <button type="button" className="btn edit-button ml-auto float-right" data-toggle="modal" data-target="#modal-signup">
               Sign up
             </button>
           </div>
