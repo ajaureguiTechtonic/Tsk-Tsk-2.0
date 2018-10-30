55dd3fb39b7d6725e2e1a519578a44caff944384
Cody Grimsley <cody.grimsley@techtonic.com>
2018-10-30 13:34:41 -0600

    Sign up modal working. btn trigger is at the bottom of the page, for now.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import TaskContainer from './components/TaskContainer';
 import LandingPage from './components/LandingPage';
 import NavBar from './components/NavBar';
 import AddTaskModal from './components/modals/AddTaskModal';
+import SignUpModal from './components/modals/SignUpModal';
 
 
 class App extends Component {
@@ -17,8 +18,9 @@ class App extends Component {
 
           <Route exact path='/' render={() => <div>
             <LandingPage />
+            <SignUpModal />
           </div>}/>
-          
+
           <Route exact path='/tasks' render={() => <div>
              <TaskContainer />
              <AddTaskModal />
