4a4acc493527cd90285fdeb02223c4dd2c760975
Ana Jauregui <ana.jauregui@techtonic.com>
2018-11-02 10:10:45 -0600

    fix react warnings

--- src/components/tasks/HigherLevelTask.js
+++ src/components/tasks/HigherLevelTask.js
@@ -19,12 +19,12 @@ class HigherLevelTask extends Component{
 
   render () {
     if (this.props.dueDate === undefined) {
-    var month = this.props.daysOld;
-    var day = 'Days Old';
-  } else {
-    var month = this.props.daysPastDue;
-    var day = 'DAYS OVERDUE';
-  };
+      var month = this.props.daysOld;
+      var day = 'Days Old';
+    } else {
+      month = this.props.daysPastDue;
+      day = 'DAYS OVERDUE';
+    };
 
     return (
       <div>
@@ -40,13 +40,12 @@ class HigherLevelTask extends Component{
                   <p className="counter">{ month } { day }</p>
                   <p className="task-name">{ this.props.taskName }</p>
                 </div>
-                {/* This is the collapseable section of the task */}
                 <Collapse isOpen = {this.state.isCollapsed} >
                   <div className="row">
-                    <div className="col-10 offset-1 col-sm-7 collapse task-description edit-this-task-${task.taskID}">
+                    <div className={`col-10 offset-1 col-sm-7 collapse task-description edit-this-task-${this.props.taskID}`}>
                       <p>{ this.props.description }</p>
                     </div>
-                    <div className="col-12 col-sm-4 collapse edit-this-task-${task.taskID}">
+                    <div className={`col-12 col-sm-4 collapse edit-this-task-${this.props.taskID}`}>
                       <div className="edit-content btn-group" role="group" aria-label="edit buttons">
                         <button type="button" className="btn edit-button listen-for-me-edit-task" onClick={this.props.handleOnEdit}>Edit</button>
                         <button type="button" className="btn edit-button listen-for-me-delete-task" onClick={this.props.handleOnDelete}>Delete</button>
@@ -57,7 +56,7 @@ class HigherLevelTask extends Component{
               </div>
             </div>
             <div className="col-1 edit-container edit-icon d-none d-sm-none d-md-block">
-              <img src={ editButton } onClick={this.toggleCollapse} />
+              <img src={ editButton } alt='' onClick={this.toggleCollapse} />
                 </div>
               </div>
             </div>
