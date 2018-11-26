802eb5a8f5958819cc3124fc1db719934e7a506b
Ursula MorenoVanderLaan <ursula.moreno-vanderlaan@techtonic.com>
2018-11-26 14:35:11 -0700

    buttons under control

--- src/components/tasks/HigherLevelTask.js
+++ src/components/tasks/HigherLevelTask.js
@@ -46,15 +46,15 @@ class HigherLevelTask extends Component{
                 </div>
                 <Collapse className="col-12" isOpen={this.state.isCollapsed} >
                   <div className="row">
-                    <div className={`col-9 offset-1 task-description edit-this-task-${this.props.taskID}`}>
+                    <div className={`col-10 offset-1 col-sm-8 offset-1 task-description edit-this-task-${this.props.taskID}`}>
                       <p>{this.props.description}</p>
                     </div>
-                    <div className={`col-2 edit-this-task-${this.props.taskID}`}>
+                    <div className={`col-2 col-sm-3 edit-buttons edit-this-task-${this.props.taskID}`}>
                       <div className="edit-content btn-group" role="group" aria-label="edit buttons">
                         <button type="button" className="btn edit-button listen-for-me-edit-task" onClick={(e) => this.props.handleOnEdit(this.props.id)}>Edit</button>
                         <button type="button" className="btn edit-button listen-for-me-delete-task" onClick={(e) => {
                           this.props.handleOnDelete(this.props.id);
-                        }}>Delete</button>
+                        }}> Delete </button>
 
                       </div>
                     </div>
