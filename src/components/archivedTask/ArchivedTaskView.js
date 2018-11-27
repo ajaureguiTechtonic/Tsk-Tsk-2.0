481eba0fb9dba4ecc0b60fbe19faada00d930d43
Ursula MorenoVanderLaan <ursula.moreno-vanderlaan@techtonic.com>
2018-11-27 15:26:40 -0700

    integrated get by user logic

--- src/components/archivedTask/ArchivedTaskView.js
+++ src/components/archivedTask/ArchivedTaskView.js
@@ -2,28 +2,38 @@ import React, { Component } from 'react';
 import TaskList from '../../../src/components/taskContainer/taskList';
 import storedTasks from '../../components/storedTasks';
 const store = require('store');
+const taskURL = 'http://127.0.0.1:4000/tsktsk';
+const axios = require('axios');
 
 class ArchivedTaskView extends Component{
   constructor (props) {
     super(props);
+    this.state = {
+      taskList: [],
+    };
 
     this.storageTasks = this.checkStorage();
     this.archivedTaskToDatabase = this.archivedTaskToDatabase.bind(this);
-
-    this.state = {
-      taskList: this.storageTasks,
-    };
   };
 
   checkStorage() {
-    if (store.get('storedTasks')) {
-      this.storageTasks = store.get('storedTasks');
+    if (this.props.isLoggedIn === true) {
+      console.log('checking storage');
+      let headers = {
+        'x-access-token': sessionStorage.getItem('jwt-token'),
+      }
+      axios.get(taskURL, { headers: headers })
+      .then((response) => {
+        console.log(response.data);
+        this.storageTasks = response.data;
+        this.setState({
+          taskList: response.data,
+        });
+      });
     } else {
-      this.storageTasks = storedTasks;
-    };
-
-    return this.storageTasks;
-  };
+      console.log("not able to bruh");
+      }
+};
 
 
 
