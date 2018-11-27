860e4d11f536ff8268d2f5b9ca21eb06af419059
Jose Hernandez-Inunza <>
2018-11-27 13:06:07 -0700

    added verify to stay logged in

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './main.css';
 import '../src/BootstrapCSS/bootstrap.min.css';
 import TaskContainer from './components/taskContainer/TaskContainer';
 import LandingPage from './components/landingPage/LandingPage';
 import NavBar from './components/navBar/NavBar';
+const authURL = 'http://127.0.0.1:4000/auth';
+const axios = require('axios');
 
 class App extends Component {
   constructor (props) {
     super(props);
+    this._verify();
     this.state = { isLoggedIn: false };
     this._logout = this._logout.bind(this);
     this._login = this._login.bind(this);
+  }
+
+  _verify() {
+    let headers = {
+      'x-access-token': sessionStorage.getItem('jwt-token'),
+    };
+
+    axios.get(`${authURL}/verify`, { headers: headers })
+    .then((jwt) => {
+      console.log(jwt);
+      if (jwt.data.auth === true) {
+        this.setState({ isLoggedIn: true });
+      } else {
+        this.setState({ isLoggedIn: false });
+      }
+    });
   }
 
   _login(jwt) {
