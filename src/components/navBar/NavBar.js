046b1c02a474207567cb36d0e3dce7f59d4c4e50
Ursula MorenoVanderLaan <ursula.moreno-vanderlaan@techtonic.com>
2018-12-03 09:39:13 -0700

    incorporated reactstrap for navbar toggles and dropdown

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -3,11 +3,24 @@ import '../../main.css';
 import './navbar.css';
 import Logo from '../../assets/first_logo.png';
 import SignUpModal from '../../components/modals/SignUpModal';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
 class NavBar extends Component {
   constructor (props) {
     super(props);
     this.state = {
+      isOpen: false,
       modal: false,
     };
     this.toggle = this.toggle.bind(this);
@@ -15,51 +28,58 @@ class NavBar extends Component {
 
   toggle() {
     this.setState({
-      modal: !this.state.modal,
+      isOpen: !this.state.isOpen,
     });
   };
 
+  loginToggle() {
+    this.setState({
+      modal: !this.state.modal,
+    })
+  }
+
   headerButton() {
     if (this.props.isLoggedIn === false) {
-      return <button type="button" onClick={this.toggle} className="btn edit-button ml-auto">Sign Up</button>
+      return (
+        <button type="button" onClick={this.loginToggle} className="btn edit-button ml-auto">Sign Up</button>
+      )
     } else if (this.props.isLoggedIn === true && this.props.location.pathname === '/archived') {
       return (
-        <ul className='navbar-nav'>
-          <li>
-            <a href="/" onClick={this.taskView} className="nav-link">Active Tasks </a>
-          </li>
-          <li>
-            <a href="/" onClick={(e) => this.props.checkLogout()} className="nav-link">Log out</a>
-          </li>
-        </ul>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="/" onClick={this.taskView}>Active Tasks</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/" onClick={(e) => this.props.checkLogout()}>Logout</NavLink>
+          </NavItem>
+        </Nav>
       )
     } else if (this.props.isLoggedIn === true && this.props.location.pathname === '/')
       return (
-        <ul className='navbar-nav'>
-          <li>
-            <a href="/archived" onClick={this.taskView} className="nav-link">Archived Tasks </a>
-          </li>
-          <li>
-            <a href="/" onClick={(e) => this.props.checkLogout()} className="nav-link">Log Out</a>
-          </li>
-        </ul>
-      )
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="/archived" onClick={this.taskView}>Archived Tasks</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/" onClick={(e) => this.props.checkLogout()}>Logout</NavLink>
+          </NavItem>
+        </Nav>
+      );
   }
 
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-sm navbar-light fixed-top">
-          <a className="navbar-brand" href="/"><img className="nav-logo" src={Logo} alt="tsk-tsk logo" /></a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-              { this.headerButton() }
-          </div>
-        </nav>
-        <SignUpModal checkLogin={this.props.checkLogin} isOpen={this.state.modal} onClick={this.toggle} />
+        <Navbar color="light" light expand="md" className="fixed-top">
+          <NavbarBrand href="/">
+            <img className="nav-logo" src={Logo} alt="tsk-tsk logo" />
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+              {this.headerButton()}
+          </Collapse>
+        </Navbar>
+        <SignUpModal checkLogin={this.props.checkLogin} isOpen={this.state.modal} onClick={this.loginToggle} />
       </div>
     );
   }
